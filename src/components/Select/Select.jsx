c08c54b23c7ea77c590a6da6e6ebb245a550a17e
Dawid Nowak <i15.nowak5@gmail.com>
2021-05-28 09:36:24 +0200

    change select options

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 
 function Select({ changeSelectValue }) {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
         <div className="select">
             <span>Wybierz ilość rund  </span>
             <select onChange={(e) => changeSelectValue(e)} defaultValue="5">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {numbers.map((item) => <option value={`${item}`}>{item}</option>)}
             </select>
         </div>
     );
